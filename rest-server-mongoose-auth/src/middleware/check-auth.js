428d179c7ac58dbcc11186d44f4ff089d3d7d585
Frederico Augusto Laranjo Silva <fredlaranjo@inatel.br>
2021-09-12 10:42:10 -0300

    Mongoose auth (Isolated issue in Store Session)

--- rest-server-mongoose-auth/src/middleware/check-auth.js
+++ rest-server-mongoose-auth/src/middleware/check-auth.js
@@ -1,7 +1,11 @@
+const base64 = require('base-64');
 const checkAuth = (request, response, next) => {
   const token = request.headers.authorization;
-
-  if(/ZG0xMjQ6bm90VzM0a1BAc3M=/.test(token)) {
+  const cookie = request.get('Cookie');
+  const notSecureData = token ? base64.decode(token.substring(6)) : null;
+  response.setHeader('Set-Cookie', 'loggedIn=false');
+  if (/ZG0xMjQ6bm90VzM0a1BAc3M=/.test(token)) {
+    response.setHeader('Set-Cookie', 'loggedIn=true')
     next();
   } else {
     const HttpStatusNotAuthorized = 401;
